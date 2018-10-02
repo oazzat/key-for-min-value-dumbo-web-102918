d4f7948f1b0b15f517cef38adc7fdfd0954363e1
Omar A <oazzat19@gmail.com>
2018-10-02 16:01:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,9 @@ def key_for_min_value(name_hash)
   comp = nil
   name_hash.each do |key,val|
     comp = val
+    if smallest_key == 0
+      smallest_key = key
+    end 
     if comp < smallest_val
       smallest_val = comp
       smallest_key = key
